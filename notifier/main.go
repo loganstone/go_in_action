ce358ee69683e8432bd5f54d82851e9f98f64acf
Logan <hs.lee.logan@gmail.com>
2019-07-09 15:45:54 +0900

    Polymorphism (p137)

--- notifier/main.go
+++ notifier/main.go
@@ -19,10 +19,25 @@ func (u *user) notify() {
 		u.emai)
 }
 
+type admin struct {
+	name string
+	emai string
+}
+
+func (a *admin) notify() {
+	fmt.Printf("Send mail for admin: %s<%s>\n",
+		a.name,
+		a.emai)
+}
+
 func main() {
 	u := user{"Logan", "hs.lee.login@gmail.com"}
 	// pointer, becouse notify() use pointer receiver
 	sendNotification(&u)
+
+	a := admin{"Hyunsuk", "blackknight8012@gmail.com"}
+	// pointer, becouse notify() use pointer receiver
+	sendNotification(&a)
 }
 
 func sendNotification(n notifier) {
